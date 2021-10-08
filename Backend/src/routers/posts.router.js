19d2032a9b48f89b4d8f1be6e0253af94065ec3c
Moshiur Rahman <mr@nuwelcom.com>
2021-10-07 23:54:17 -0300

    starting

--- Backend/src/routers/posts.router.js
+++ Backend/src/routers/posts.router.js
@@ -34,7 +34,6 @@ router.get("/:id", async (req, res) => {
 //update a post
 
 router.put("/:id", async (req, res) => {
-	res.header("Access-Control-Allow-Origin", "*");
 	try {
 		const post = await Post.findById(req.params.id);
 
